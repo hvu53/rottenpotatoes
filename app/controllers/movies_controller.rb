abe8510f11ceece2b00630523e349631f70e4755
Hoa Vu <hoavt040789@gmail.com>
2013-08-01 10:46:01 -0700

    checkbox revert

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,57 +8,36 @@ class MoviesController < ApplicationController
 
   def index
     # @movies = Movie.order(params[:sort])
-    # if params[:commit] == 'Refresh'
-    #   session[:ratings] = params[:ratings]
-    # elsif session[:ratings] != params[:ratings]
-    #   redirect = true
-    #   params[:ratings] = session[:ratings]
-    # end
-
-    # if params[:sort]
-    #   session[:sort] = params[:sort]
-    # elsif session[:sort]
-    #   redirect = true
-    #   params[:sort] = session[:sort]
-    # end
-
-    # @ratings = session[:ratings]
-    # @sort = session[:sort]
-
-    # if redirect
-    #   redirect_to movies_path({:sort => @sort, :ratings => @ratings})
-    # elsif columns = {'title' => 'title', 'release_date' => 'release_date'}
-    #   if columns.has_key?(@sort)
-    #     query = Movie.order(columns[@sort])
-    #   else
-    #     @sort = nil
-    #     query = Movie
-    #   end
-
-    #   @movies = @ratings.nil? ? query.all : query.find_all_by_rating(@ratings.map { |r| r[0]} )
-    #   @all_ratings = Movie.ratings
-    # end
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'title'
-      ordering,@title_header = {:order => :title}, 'hilite'
-    when 'release_date'
-      ordering,@date_header = {:order => :release_date}, 'hilite'
-    end
-    @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || session[:ratings] || {}
-
-    if params[:sort] != session[:sort]
-      session[:sort] = sort
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    if params[:commit] == 'Refresh'
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings] != params[:ratings]
+      redirect = true
+      params[:ratings] = session[:ratings]
     end
 
-    if params[:ratings] != session[:ratings] and @selected_ratings != {}
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    if params[:sort]
+      session[:sort] = params[:sort]
+    elsif session[:sort]
+      redirect = true
+      params[:sort] = session[:sort]
     end
-    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
+
+    @ratings = session[:ratings]
+    @sort = session[:sort]
+
+    if redirect
+      redirect_to movies_path({:sort => @sort, :ratings => @ratings})
+    elsif columns = {'title' => 'title', 'release_date' => 'release_date'}
+      if columns.has_key?(@sort)
+        query = Movie.order(columns[@sort])
+      else
+        @sort = nil
+        query = Movie
+      end
+
+      @movies = @ratings.nil? ? query.all : query.find_all_by_rating(@ratings.map { |r| r[0]} )
+      @all_ratings = Movie.ratings
+    end
   end
 
   def new
